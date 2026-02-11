03737a5b2b5efd759c48d253e35a36ab1f8a6d0b
Matúš Čongrády <matus.congrady@stacktape.com>
2026-02-11 04:35:17 +0100

    Update README.md

---
+++ stacktape.ts
@@ -0,0 +1,23 @@
+import { CustomDockerfilePackaging, WebService, defineConfig } from 'stacktape';
+
+export default defineConfig(() => {
+  const webService = new WebService({
+    packaging: new CustomDockerfilePackaging({
+      buildContextPath: './'
+    }),
+    resources: {
+      cpu: 0.25,
+      memory: 512
+    },
+    cors: {
+      enabled: true
+    },
+    cdn: {
+      enabled: true
+    }
+  });
+
+  return {
+    resources: { webService }
+  };
+});
